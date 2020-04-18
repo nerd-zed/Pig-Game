fbdbc8191af42a07aea7ab0a6b40e2fa6f31a2d8
nerd-zed <sdeepakraja1997@gmail.com>
2020-04-18 12:34:00 +0530

    Created a Pig Game

---
+++ app.js
@@ -0,0 +1,101 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, randomScore, activePlayer, isGameOver;
+
+init();
+
+/* var x = document.querySelector('#score-0').textContent;
+console.log(x); */
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if (isGameOver === false) {
+        // Generate random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+    
+        //select the correct dice image
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+    
+        if(dice != 1)
+        {    // change the current score
+             randomScore += dice;
+             document.getElementById('current-' + activePlayer).textContent = randomScore;
+         
+        }
+        else {  
+            nextPlayer();
+        
+        }
+    }
+    
+    
+    
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (isGameOver === false) {
+        scores[activePlayer] += randomScore;
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= 20)
+        {
+            document.getElementById('name-' + activePlayer).textContent = 'winner';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            document.querySelector('.dice').style.display = 'none';
+            isGameOver = true;
+        }
+    else nextPlayer();
+    }
+    
+
+
+});
+
+function nextPlayer()
+{
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+        document.querySelector('.dice').style.display = 'none';
+        randomScore = 0;
+}
+
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    isGameOver = false;
+    scores = [0, 0];
+    randomScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#score-0').textContent = 0;
+    document.querySelector('#score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+
+}
+
+
+document.querySelector('.btn-info').addEventListener('click',function() {
+    alert('GAME RULES:\n- The game has 2 players, playing in rounds\n- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score\n- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it\'s the next player\'s turn\n- The player can choose to \'Hold\', which means that his ROUND score gets added to his GLBAL score. After that, it\'s the next player\'s turn\n- The first player to reach 100 points on GLOBAL score wins the game');
+});
